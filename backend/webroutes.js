314326e1722f14b6f0c055b36d27821e24615aac
sachinsinghshah <sachinsinghshah@gmail.com>
2024-10-13 02:03:16 +0530

    added boilerplate for routes

--- backend/webroutes.js
+++ backend/webroutes.js
@@ -1,23 +1,37 @@
-import express, { json } from "express";
+const express = require("express")
+const cors = require("cors")
 const app = express();
-app.use(json());
+
+app.use(express.json());
+app.use(cors())
 // Home
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
   console.log("Home page");
-  res.json({ message: "Welcome to the home page" });
+  res.json({ 
+    message: "Welcome to the home page" 
+  });
 });
 
 // Glimpse
 app.get("/glimpse", (req, res) => {
-  res.json({ message: "Welcome to the glimpse page" });
+  res.json({
+    message: "Welcome to the glimpse page" 
+  });
 });
+
 // Highlights
+
 // The Company
+
 // Our Services
+
 // Works
+
 // Our Clients
+
 // Contacts
 
+// Start the server
 app.listen(3000, () => {
-  console.log("Server is running on port 3000 - https://localHost:3000");
+  console.log("Server is running on port 3000 - http://localhost:3000");
 });
